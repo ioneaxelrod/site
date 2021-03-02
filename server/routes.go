d70aa7a1a2bdddf49bd38535bbb70e3d4800c523
Ione Axelrod <ioneaxelrod@gmail.com>
2021-03-02 14:30:19 -0800

    Removed main binary and added it to gitignore

--- server/routes.go
+++ server/routes.go
@@ -6,16 +6,51 @@ func setUpRouter() *gin.Engine {
   r := gin.Default()
   r.GET("/ping", ping)
   r.GET("/", homePage)
+  r.GET("/about", about)
+  r.GET("/resume", resume)
+  r.GET("/interests", interests)
+  r.GET("/art", art)
+  r.GET("/donate", donate)
+
   return r
 }
 
 func homePage(c *gin.Context) {
     c.JSON(200, gin.H{
-      "message": "poop",
+      "message": "Home",
     })
 }
 
-// Ping is a test endpoint
+func about(c * gin.Context) {
+  c.JSON(200, gin.H{
+    "message": "About",
+  })
+}
+
+func resume(c * gin.Context) {
+  c.JSON(200, gin.H{
+    "message": "Resume",
+  })
+}
+
+func interests(c * gin.Context) {
+  c.JSON(200, gin.H{
+    "message": "Interests",
+  })
+}
+
+func art(c * gin.Context) {
+  c.JSON(200, gin.H{
+    "message": "Art",
+  })
+}
+
+func donate(c * gin.Context) {
+  c.JSON(200, gin.H{
+    "message": "Donate",
+  })
+}
+
 func ping(c *gin.Context) {
   c.JSON(200, gin.H{
     "message": "pong",
